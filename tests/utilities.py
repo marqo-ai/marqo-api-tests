262940a7cc9e42a5f440e01759a56ad31c7ce879
root <root@JKM-DESKTOP.localdomain>
2022-11-15 23:23:52 +0800

    added allowed env decorator, cuda tox env, prototype cuda tests

--- tests/utilities.py
+++ tests/utilities.py
@@ -3,7 +3,7 @@ import typing
 
 
 def disallow_environments(disallowed_configurations: typing.List[str]):
-    """This construct that wraps a test ensures it only runs for allowed
+    """This construct wraps a test to ensure that it does not run for disallowed 
     testing environments.
 
     It figures by examining the "TESTING_CONFIGURATION" environment variable.
@@ -16,6 +16,28 @@ def disallow_environments(disallowed_configurations: typing.List[str]):
     def decorator(function):
         def wrapper(*args, **kwargs):
             if os.environ["TESTING_CONFIGURATION"] in disallowed_configurations:
+                return
+            else:
+                result = function(*args, **kwargs)
+                return result
+        return wrapper
+    return decorator
+
+
+def allow_environments(allowed_configurations: typing.List[str]):
+    """This construct wraps a test to ensure it only runs for allowed
+    testing environments.
+
+    It figures by examining the "TESTING_CONFIGURATION" environment variable.
+
+    Args:
+        allowed_configurations: if the environment variable
+        "TESTING_CONFIGURATION" matches a configuration in
+        allowed_configurations, then the test will be run
+    """
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            if os.environ["TESTING_CONFIGURATION"] not in allowed_configurations:
                 return
             else:
                 result = function(*args, **kwargs)
