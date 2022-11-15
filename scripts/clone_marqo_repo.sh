48886038a09de8ca6343626bb1a6ef544d54a000
root <root@JKM-DESKTOP.localdomain>
2022-11-15 19:38:42 +0800

    added option for other hardcoded repo to clone. added cuda script

--- scripts/clone_marqo_repo.sh
+++ scripts/clone_marqo_repo.sh
@@ -8,7 +8,7 @@ export MARQO_API_TESTS_ROOT=$(pwd)
 export LOCAL_OPENSEARCH_URL="https://localhost:9200"
 
 # Removing pwd check.
-pwd
+# Might be better as $(pwd | grep -v actions-runner/_work)
 # if [[ $(pwd | grep -v marqo-api-tests) && $(pwd | grep -v runner/work) ]]; then
 #  echo checked pwd, and it does not look the directory is correct
 #  exit 1
@@ -16,7 +16,10 @@ pwd
 rm -rf "${MARQO_API_TESTS_ROOT}/temp"
 mkdir "${MARQO_API_TESTS_ROOT}/temp"
 cd "${MARQO_API_TESTS_ROOT}/temp" || exit
+# TODO, change this back. For now, I'll direct it to the url of my fork
+# git clone https://github.com/vicilliar/marqo-CI-tests.git
 git clone https://github.com/marqo-ai/marqo.git
+
 cd "${MARQO_API_TESTS_ROOT}/temp/marqo" || exit
 git fetch
 git switch "$2"
