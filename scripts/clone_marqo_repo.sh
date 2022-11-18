0a47d6f437fc68431441b8d2220fcf9ae04752f1
root <root@JKM-DESKTOP.localdomain>
2022-11-18 15:53:44 +0800

    removed pwd check from clone

--- scripts/clone_marqo_repo.sh
+++ scripts/clone_marqo_repo.sh
@@ -7,12 +7,12 @@ export MARQO_API_TESTS_ROOT=$(pwd)
 . "${MARQO_API_TESTS_ROOT}/conf"
 export LOCAL_OPENSEARCH_URL="https://localhost:9200"
 
-
+# Removing pwd check.
 # Might be better as $(pwd | grep -v actions-runner/_work)
- if [[ $(pwd | grep -v marqo-api-tests) && $(pwd | grep -v runner/work) && $(pwd | grep -v actions-runner/_work)]]; then
-  echo checked pwd, and it does not look the directory is correct
-  exit 1
- fi
+# if [[ $(pwd | grep -v marqo-api-tests) && $(pwd | grep -v runner/work) ]]; then
+#  echo checked pwd, and it does not look the directory is correct
+#  exit 1
+# fi
 rm -rf "${MARQO_API_TESTS_ROOT}/temp"
 mkdir "${MARQO_API_TESTS_ROOT}/temp"
 cd "${MARQO_API_TESTS_ROOT}/temp" || exit
