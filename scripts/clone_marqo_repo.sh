c5207fae70d90be0b372bec9bd5e49f549152a53
root <root@JKM-DESKTOP.localdomain>
2022-11-18 17:59:52 +0800

    updated comments

--- scripts/clone_marqo_repo.sh
+++ scripts/clone_marqo_repo.sh
@@ -7,17 +7,17 @@ export MARQO_API_TESTS_ROOT=$(pwd)
 . "${MARQO_API_TESTS_ROOT}/conf"
 export LOCAL_OPENSEARCH_URL="https://localhost:9200"
 
-# Removing pwd check.
-# Might be better as $(pwd | grep -v actions-runner/_work)
+# NOTE: Removing pwd check.
+# TODO: Maybe implement  $(pwd | grep -v actions-runner/_work)
 # if [[ $(pwd | grep -v marqo-api-tests) && $(pwd | grep -v runner/work) ]]; then
 #  echo checked pwd, and it does not look the directory is correct
 #  exit 1
 # fi
+
 rm -rf "${MARQO_API_TESTS_ROOT}/temp"
 mkdir "${MARQO_API_TESTS_ROOT}/temp"
 cd "${MARQO_API_TESTS_ROOT}/temp" || exit
-# TODO, change this back. For now, I'll direct it to the url of my fork
-# git clone https://github.com/vicilliar/marqo-CI-tests.git
+
 git clone https://github.com/marqo-ai/marqo.git
 
 cd "${MARQO_API_TESTS_ROOT}/temp/marqo" || exit
