c090017a40c540625b3780ebd38617b082e73bb4
joshuak <joshua@marqo.ai>
2023-05-08 16:48:00 +0800

    added dummy throttling test file

---
+++ tests/test_throttling.py
@@ -0,0 +1,31 @@
+import copy
+import pprint
+from marqo.client import Client
+from marqo.errors import MarqoApiError, MarqoError, MarqoWebError
+import unittest
+from tests.marqo_test import MarqoTestCase
+from marqo import enums
+from unittest import mock
+
+
+class TestThrottling(MarqoTestCase):
+
+    def setUp(self) -> None:
+        self.client = Client(**self.client_settings)
+        self.index_name_1 = "my-test-index-1"
+        try:
+            self.client.delete_index(self.index_name_1)
+        except MarqoApiError as s:
+            pass
+
+    def tearDown(self) -> None:
+        try:
+            self.client.delete_index(self.index_name_1)
+        except MarqoApiError as s:
+            pass
+        
+    """
+    def test_max_throttle(self):
+        self.client.create_index(index_name=self.index_name_1)
+        pass
+    """
