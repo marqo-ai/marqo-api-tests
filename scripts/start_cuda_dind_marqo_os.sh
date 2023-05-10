c8c2a97075984eff2d7524c41d6cba423a5e02a4
joshuak <joshua@marqo.ai>
2023-05-10 12:30:45 +0800

    testing adding space

--- scripts/start_cuda_dind_marqo_os.sh
+++ scripts/start_cuda_dind_marqo_os.sh
@@ -4,7 +4,7 @@
 # $2 : env_vars - string representing the env vars to pass marqo
 
 docker rm -f marqo;
-     docker run --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway "$1" &
+     docker run --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway  "$1" &
 # wait for marqo to start
 until [[ $(curl -v --silent --insecure http://localhost:8882 2>&1 | grep Marqo) ]]; do
     sleep 0.1;
