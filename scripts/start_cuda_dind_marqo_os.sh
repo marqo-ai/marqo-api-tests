9f7efc212e0985896cf17f3487dbc35d0437d730
joshuak <joshua@marqo.ai>
2023-05-10 16:18:13 +0800

    changed cuda 2 formula

--- scripts/start_cuda_dind_marqo_os.sh
+++ scripts/start_cuda_dind_marqo_os.sh
@@ -4,7 +4,7 @@
 # $2 : env_vars - string representing the env vars to pass marqo
 set -x
 docker rm -f marqo;
-     docker run --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway "$2" "$1" &
+     docker run --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway ${2:+"$2"} "$1" &
 set +x
 # wait for marqo to start
 until [[ $(curl -v --silent --insecure http://localhost:8882 2>&1 | grep Marqo) ]]; do
