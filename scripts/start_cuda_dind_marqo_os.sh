4cbae6c634a399f54239f977a5d8370b821167b7
joshuak <joshua@marqo.ai>
2023-05-22 16:40:58 +0800

    changed debug messages

--- scripts/start_cuda_dind_marqo_os.sh
+++ scripts/start_cuda_dind_marqo_os.sh
@@ -6,19 +6,21 @@ set -x
 docker rm -f marqo;
 # TODO put this back (remove models to preload)
 # docker run --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway ${2:+"$2"} "$1" &
-docker run -d --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway -e MARQO_MODELS_TO_PRELOAD='[]' ${2:+"$2"} "$1" &
+docker run -d --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway -e MARQO_MODELS_TO_PRELOAD='[]' ${2:+"$2"} "$1"
 
 # Follow the logs of the Docker container in the background and capture its PID
 docker logs -f marqo &
 LOGS_PID=$!
 
 # wait for marqo to start
+set +x
 until [[ $(curl -v --silent --insecure http://localhost:8882 2>&1 | grep Marqo) ]]; do
     sleep 1;    # TODO: change back to 0.1
     echo "Still waiting for marqo to start";
 done;
-
+set -x
 # Kill the `docker logs` command now that we're done with everything else
 kill $LOGS_PID
-set +x
+
 echo "Finished script.";
+set -x
