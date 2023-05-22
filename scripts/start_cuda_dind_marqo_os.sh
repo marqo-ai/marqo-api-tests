e3d8f50787c121aa96294ec0d69512488e8adf89
joshuak <joshua@marqo.ai>
2023-05-22 14:55:44 +0800

    disown instead of detach docker

--- scripts/start_cuda_dind_marqo_os.sh
+++ scripts/start_cuda_dind_marqo_os.sh
@@ -4,13 +4,17 @@
 # $2 : env_vars - string representing the env vars to pass marqo
 set -x
 docker rm -f marqo;
-     # TODO put this back (remove models to preload)
-     # docker run --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway ${2:+"$2"} "$1" &
-     docker run -d --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway -e MARQO_MODELS_TO_PRELOAD='[]' ${2:+"$2"} "$1" &
+# TODO put this back (remove models to preload)
+# docker run --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway ${2:+"$2"} "$1" &
+docker run --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway -e MARQO_MODELS_TO_PRELOAD='[]' ${2:+"$2"} "$1" &
+DOCKER_PID=$!
 set +x
 # wait for marqo to start
 until [[ $(curl -v --silent --insecure http://localhost:8882 2>&1 | grep Marqo) ]]; do
     sleep 1;    # TODO: change back to 0.1
     echo "Still waiting for marqo to start";
 done;
+
+# Disown the Docker process now that we're done with everything else
+disown $DOCKER_PID
 echo "Finished script.";
