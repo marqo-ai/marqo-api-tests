ec54ccea2dda6bc36f582bac6b7bfca3b1581b6d
joshuak <joshua@marqo.ai>
2023-05-22 19:11:38 +0800

    stopped using list method

--- scripts/start_cuda_dind_marqo_os.sh
+++ scripts/start_cuda_dind_marqo_os.sh
@@ -6,26 +6,26 @@ set -x
 docker rm -f marqo;
 
 # docker call args array
-declare -a args
-args=("--name" "marqo" "--gpus" "all" "--privileged" "-p" "8882:8882" "--add-host" "host.docker.internal:host-gateway")
+# declare -a args
+#args=("--name" "marqo" "--gpus" "all" "--privileged" "-p" "8882:8882" "--add-host" "host.docker.internal:host-gateway")
 
 # add $2 env vars if it's set
-if [ -n "$2" ]; then
-    echo "checking arg2: $2"
-    # split $2 into an array on spaces
-    IFS=' ' read -r -a env_vars <<< "$2"
-    # add each item in env_vars to args
-    for var in "${env_vars[@]}"; do
-        args+=("$var")
-    done
-fi
+#if [ -n "$2" ]; then
+#    echo "checking arg2: $2"
+#    # split $2 into an array on spaces
+#    IFS=' ' read -r -a env_vars <<< "$2"
+#    # add each item in env_vars to args
+#    for var in "${env_vars[@]}"; do
+#        args+=("$var")
+#    done
+#fi
 
 # add $1 marqo image name to args
-args+=("$1")
-docker run -d "${args[@]}"
+#args+=("$1")
+#docker run -d "${args[@]}"
 
 # TODO put this back (remove models to preload)
-# docker run --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway ${2:+"$2"} "$1"
+docker run --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway ${2:+$2} "$1"
 
 # Follow the logs of the Docker container in the background and capture its PID
 docker logs -f marqo &
