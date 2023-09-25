23f0b339bb51a2e83d0c26139bed129bd2e00021
pandu-k <pand@s2search.io>
2023-09-25 10:03:28 +1000

    remove TestLogOutPut sublclassing TestEnvVarChanges

--- tests/application_tests/test_log_output.py
+++ tests/application_tests/test_log_output.py
@@ -2,12 +2,28 @@ from tests import marqo_test
 from tests import utilities
 from marqo import Client
 from marqo.errors import MarqoApiError, BackendCommunicationError, MarqoWebError
-import pprint
-import json
-from tests.application_tests.test_env_var_changes import  TestEnvVarChanges
+from tests.application_tests.test_env_var_changes import TestEnvVarChanges
+from tests import marqo_test
 
 
-class TestLogOutPut(TestEnvVarChanges):
+class TestLogOutPut(marqo_test.MarqoTestCase):
+
+    def setUp(self) -> None:
+        self.client = Client(**self.client_settings)
+        self.index_name_1 = "my-test-index-1"
+        try:
+            self.client.delete_index(self.index_name_1)
+        except MarqoApiError as s:
+            pass
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        super().tearDownClass()
+        # Ensures that marqo goes back to default state after these tests
+        utilities.rerun_marqo_with_default_config(
+            calling_class=cls.__name__
+        )
+        print("Marqo has been rerun with default env vars!")
 
     def test_log_output_default(self):
         """Marqo should default to info level"""
@@ -15,6 +31,7 @@ class TestLogOutPut(TestEnvVarChanges):
             env_vars=["-e", f"MARQO_LOG_LEVEL=INFO"],
             calling_class=self.__class__.__name__
         )
+        print("test_log_output_defaulttest_log_output_defaulttest_log_output_defaulttest_log_output_default")
         for l in process_object:
             print(l)
 
