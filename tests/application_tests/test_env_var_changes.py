e33e0b0ebc0b75eb65d26115eef9d53f3bc2a4c7
pandu-k <107458762+pandu-k@users.noreply.github.com>
2023-09-28 17:55:04 +1000

    Docker log tests (#49)
    Added tests to read docker output and confirm startup output is squashed

--- tests/application_tests/test_env_var_changes.py
+++ tests/application_tests/test_env_var_changes.py
@@ -1,14 +1,28 @@
-import logging
-import subprocess
-import os
-import time
-from requests import HTTPError
+"""
+
+To test these functions locally:
+
+1. Run a Marqo container another terminal (these tests assume there is a running Marqo
+    container and then try to kill it, failing if unsuccessful)
+2. cd into the root of this repo
+3. Run the following command (you can replace MARQO_IMAGE_NAME):
+
+    TESTING_CONFIGURATION=DIND_MARQO_OS \
+    MARQO_API_TESTS_ROOT=. \
+    MARQO_IMAGE_NAME=marqoai/marqo:latest \
+    pytest tests/application_tests/test_env_var_changes.py::TestEnvVarChanges::test_multiple_env_vars
+
+
+We may test multiple different env vars in the same test case. This is because
+ each new test case is expensive, requiring a restart of Marqo. This prevents
+ this test suite's runtime from growing too large.
+"""
 from tests import marqo_test
 from tests import utilities
 from marqo import Client
-from marqo.errors import MarqoApiError, BackendCommunicationError, MarqoWebError
-import pprint
+from marqo.errors import MarqoApiError, MarqoWebError
 import json
+
 
 class TestEnvVarChanges(marqo_test.MarqoTestCase):
 
@@ -35,12 +49,18 @@ class TestEnvVarChanges(marqo_test.MarqoTestCase):
         print("Marqo has been rerun with default env vars!")
 
     def test_max_replicas(self):
-        # Default max is 1
-        # Rerun marqo with new replica count
+        """
+        Tests Marqo with a different max replica count to default (Default max is 1).
+
+        Also, tests that INFO-level log output is as expected.
+        """
         max_replicas = 5
-        print(f"Attempting to rerun marqo with max replicas: {max_replicas}")
+        log_level = 'info'
+        print(f"Attempting to rerun marqo with max replicas: {max_replicas} and log level {log_level}")
         utilities.rerun_marqo_with_env_vars(
-            env_vars = ["-e", f"MARQO_MAX_NUMBER_OF_REPLICAS={max_replicas}"],
+            env_vars=[
+                "-e", f"MARQO_MAX_NUMBER_OF_REPLICAS={max_replicas}",
+                "-e", f"MARQO_LOG_LEVEL={log_level}"],
             calling_class=self.__class__.__name__
         )
 
@@ -56,11 +76,35 @@ class TestEnvVarChanges(marqo_test.MarqoTestCase):
         # Make sure new index has 4 replicas
         assert self.client.get_index(self.index_name_1).get_settings() \
             ["number_of_replicas"] == 4
-    
+
+        # ## Testing log output when LEVEL=info ##
+        #    we want to ensure that no excessive log messages are printed
+        log_blob = utilities.retrieve_docker_logs(container_name='marqo')
+        assert ("Unverified HTTPS request is being made to host 'host.docker.internal'. "
+                "Adding certificate verification is strongly advised." not in log_blob)
+
+        assert ("Unverified HTTPS request is being made to host 'localhost'. "
+                "Adding certificate verification is strongly advised." not in log_blob)
+        assert 'torch==' not in log_blob
+        assert 'Status: Downloaded newer image for marqoai/marqo-os' not in log_blob
+        assert 'FutureWarning: Importing `GenerationMixin`' not in log_blob
+        assert 'Called redis-server command' not in log_blob
+
+        # to assure use that logs aren't just completely empty:
+        assert 'COMPLETED SUCCESSFULLY' in log_blob
+        assert 'Marqo throttling successfully started.' in log_blob
+        assert 'INFO:DeviceSummary:found devices' in log_blob
+        assert 'INFO:ModelsForStartup:completed loading models' in log_blob
+        # ## End log output tests ##
 
     def test_preload_models(self):
-        # Default models are ["hf/all_datasets_v4_MiniLM-L6", "ViT-L/14"]
-        # Rerun marqo with new custom model
+        """
+        Tests rerunning marqo with non-default, custom model.
+        Default models are ["hf/all_datasets_v4_MiniLM-L6", "ViT-L/14"]
+
+        Also, this tests log output when log level is not set. The log level should be INFO by default.
+        """
+
         open_clip_model_object = {
             "model": "open-clip-1",
             "model_properties": {
@@ -82,6 +126,25 @@ class TestEnvVarChanges(marqo_test.MarqoTestCase):
         res = self.client.get_loaded_models()
         assert set([item["model_name"] for item in res["models"]]) == set(custom_models)
 
+        # ## Testing log output when log level is not set. ##
+        #    we want to ensure that no excessive log messages are printed
+        log_blob = utilities.retrieve_docker_logs(container_name='marqo')
+        assert ("Unverified HTTPS request is being made to host 'host.docker.internal'. "
+                "Adding certificate verification is strongly advised." not in log_blob)
+
+        assert  ("Unverified HTTPS request is being made to host 'localhost'. "
+                 "Adding certificate verification is strongly advised." not in log_blob)
+        assert 'torch==' not in log_blob
+        assert 'Status: Downloaded newer image for marqoai/marqo-os' not in log_blob
+        assert  'FutureWarning: Importing `GenerationMixin`' not in log_blob
+        assert 'Called redis-server command' not in log_blob
+
+        # to assure use that logs aren't just completely empty:
+        assert 'COMPLETED SUCCESSFULLY' in log_blob
+        assert 'Marqo throttling successfully started.' in log_blob
+        assert 'INFO:DeviceSummary:found devices' in log_blob
+        assert 'INFO:ModelsForStartup:completed loading models' in log_blob
+        # ## End log output tests ##
 
     def test_multiple_env_vars(self):
         """
@@ -92,6 +155,9 @@ class TestEnvVarChanges(marqo_test.MarqoTestCase):
             1. Load models
             2. set max number of replicas
             3. set max EF
+            4. set log level to debug
+
+            Also, asserts that Marqo's debug output is as expected
         """
 
         # Restart marqo with new max values
@@ -99,22 +165,23 @@ class TestEnvVarChanges(marqo_test.MarqoTestCase):
         max_ef = 6000
         new_models = ["hf/all_datasets_v4_MiniLM-L6"]
         utilities.rerun_marqo_with_env_vars(
-            env_vars = [
+            env_vars=[
                 "-e", f"MARQO_MAX_NUMBER_OF_REPLICAS={max_replicas}",
                 "-e", f"MARQO_EF_CONSTRUCTION_MAX_VALUE={max_ef}",
-                "-e", f"MARQO_MODELS_TO_PRELOAD={json.dumps(new_models)}"
+                "-e", f"MARQO_MODELS_TO_PRELOAD={json.dumps(new_models)}",
+                "-e", f"MARQO_LOG_LEVEL=debug"
             ],
             calling_class=self.__class__.__name__
         )
 
         # Create index with same number of replicas and EF
         res_0 = self.client.create_index(index_name=self.index_name_1, settings_dict={
-            "number_of_replicas": 4,         # should be fine now
+            "number_of_replicas": 4,  # should be fine now
             "index_defaults": {
-                "ann_parameters" : {
+                "ann_parameters": {
                     "space_type": "cosinesimil",
                     "parameters": {
-                        "ef_construction": 5000,    # should be fine now
+                        "ef_construction": 5000,  # should be fine now
                         "m": 16
                     }
                 }
@@ -124,16 +191,38 @@ class TestEnvVarChanges(marqo_test.MarqoTestCase):
         # Assert correct replicas
         # Make sure new index has 4 replicas
         assert self.client.get_index(self.index_name_1).get_settings() \
-            ["number_of_replicas"] == 4
-        
+                   ["number_of_replicas"] == 4
+
         # Assert correct EF const
         assert self.client.get_index(self.index_name_1).get_settings() \
-            ["index_defaults"]["ann_parameters"]["parameters"]["ef_construction"] == 5000
+                   ["index_defaults"]["ann_parameters"]["parameters"]["ef_construction"] == 5000
 
         # Assert correct models
         res = self.client.get_loaded_models()
         assert set([item["model_name"] for item in res["models"]]) == set(new_models)
 
+        # ## Testing log output when LEVEL=debug ##
+        #    we want to ensure that  in debug mode, no information is hidden
+
+        # use the index to generate more log outputs (specifically regarding HTTPS requests)
+        self.client.index(self.index_name_1).add_documents(
+            documents=[{'Title': 'Recipes for hippos'}],
+            tensor_fields=['Title'],
+            auto_refresh=True
+        )
+        self.client.index(self.index_name_1).search('something')
+        log_blob = utilities.retrieve_docker_logs(container_name='marqo')
+        assert 'torch==' in log_blob
+        # assertions about HTTP requests aren't reliable for some reason
+        # assert ((
+        #                 "Unverified HTTPS request is being made to host 'host.docker.internal'. "
+        #                 "Adding certificate verification is strongly advised." in log_blob)
+        #         or (
+        #                 "Unverified HTTPS request is being made to host 'localhost'. "
+        #                 "Adding certificate verification is strongly advised." in log_blob))
+        assert 'Status: Downloaded newer image for marqoai/marqo-os' in log_blob
+        assert 'FutureWarning: Importing `GenerationMixin`' in log_blob
+        assert 'Called redis-server command' in log_blob
 
     def test_max_add_docs_count(self):
         """
@@ -176,3 +265,4 @@ class TestEnvVarChanges(marqo_test.MarqoTestCase):
                 raise AssertionError("Add docs call should have failed with bad request")
             except MarqoWebError as e:
                 assert e.code == "bad_request"
+
--- tests/utilities.py
+++ tests/utilities.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import typing
+import threading
+import inspect
 
 
 def disallow_environments(disallowed_configurations: typing.List[str]):
@@ -58,7 +60,8 @@ def rerun_marqo_with_env_vars(env_vars: list = [], calling_class: str = ""):
     """
 
     if calling_class not in ["TestEnvVarChanges"]:
-        raise RuntimeError(f"Rerun Marqo function should only be called by `TestEnvVarChanges` to ensure other API tests are not affected. Given calling class is {calling_class}")
+        raise RuntimeError(
+            f"Rerun Marqo function should only be called by `TestEnvVarChanges` to ensure other API tests are not affected. Given calling class is {calling_class}")
 
     # Stop Marqo
     print("Attempting to stop marqo.")
@@ -67,7 +70,7 @@ def rerun_marqo_with_env_vars(env_vars: list = [], calling_class: str = ""):
 
     # Rerun the appropriate start script
     test_config = os.environ["TESTING_CONFIGURATION"]
-    
+
     if test_config == "LOCAL_MARQO_OS":
         start_script_name = "start_local_marqo_os.sh"
     elif test_config == "DIND_MARQO_OS":
@@ -82,19 +85,19 @@ def rerun_marqo_with_env_vars(env_vars: list = [], calling_class: str = ""):
 
     run_process = subprocess.Popen(
         [
-            "bash",                             # command: run
-            full_script_path,                   # script to run
-            os.environ['MARQO_IMAGE_NAME'],     # arg $1 in script
-        ] + env_vars,                           # args $2 onwards
-        stdout=subprocess.PIPE, 
-        stderr=subprocess.STDOUT, 
+            "bash",  # command: run
+            full_script_path,  # script to run
+            os.environ['MARQO_IMAGE_NAME'],  # arg $1 in script
+        ] + env_vars,  # args $2 onwards
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
         universal_newlines=True
     )
-    
+
     # Read and print the output line by line (in real time)
     for line in run_process.stdout:
         print(line, end='')
-    
+
     # Wait for the process to complete
     run_process.wait()
     return True
@@ -104,3 +107,62 @@ def rerun_marqo_with_default_config(calling_class: str = ""):
     # Do not send any env vars
     # This should act like running the start script at the beginning
     rerun_marqo_with_env_vars(env_vars=[], calling_class=calling_class)
+
+
+docker_log_failure_message = "Failed to fetch docker logs for Marqo"
+
+
+def attach_docker_logs(container_name: str, log_collection: typing.List) -> None:
+    """Fetches the Docker logs of a specified container and stores them in a provided list.
+
+    Args:
+        container_name (str): Name of the Docker container whose logs are to be
+            fetched.
+        log_collection (List): A list which its first element is used to store
+            the fetched logs or error messages. A list is used to store the
+            logs, rather than returning them, so this function can be used in a
+            thread.
+    """
+    completed_process = subprocess.run(
+        ["docker", "logs", container_name],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True
+    )
+
+    if completed_process.returncode == 0:
+        log_collection.append(completed_process.stdout)
+    else:
+        log_collection.append(
+            f"{docker_log_failure_message}. "
+            f"Failed with error: {completed_process.stderr}")
+
+
+def retrieve_docker_logs(
+        container_name: str
+) -> str:
+    """Returns docker logs as a string, for a specific container
+
+    Args:
+        container_name (str): Name of the Docker container whose logs are to be checked. Defaults to 'marqo'.
+    Returns:
+        A str which is the docker logs for the container.
+    Raises:
+        RuntimeError: If fetching logs fails or times out.
+    """
+    # a 1-elem mutable object to save the docker logs to:
+    log_collection = []
+
+    docker_log_fetcher = attach_docker_logs
+
+    docker_log_fetcher(container_name=container_name, log_collection=log_collection)
+
+    if not log_collection:
+        raise RuntimeError(
+            "Fetching logs timed out or failed. log_collection is empty.")
+
+    if docker_log_failure_message in log_collection[0]:
+        raise RuntimeError(f"{docker_log_fetcher.__name__} encountered an "
+                           f"error retrieving docker logs. {log_collection[0]}")
+
+    return log_collection[0]
