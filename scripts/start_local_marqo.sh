98bab2000230f1f2b5fbdc64aad72b44b6c1a7cc
Li Wan <lwan3@student.unimelb.edu.au>
2024-01-19 14:02:35 +1100

    Fix start local marqo

--- scripts/start_local_marqo.sh
+++ scripts/start_local_marqo.sh
@@ -9,19 +9,19 @@ python3 scripts/start_vespa.py
 MARQO_DOCKER_IMAGE="$1"
 shift
 
-docker rm -f marqo
+docker rm -f marqo 2>/dev/null || true
 
 # Explanation:
 # -d detaches docker from process (so subprocess does not wait for it)
 # ${@:+"$@"} adds ALL args (past $1) if any exist.
 
 set -x
-docker run -d --name marqo --privileged -p 8882:8882 \
-    -e "MARQO_MAX_CPU_MODEL_MEMORY=1.6" \
-    -e "MARQO_ENABLE_BATCH_APIS=true" \
-    -e "VESPA_CONFIG_URL=http://localhost:19071" \
-    -e "VESPA_DOCUMENT_URL=http://localhost:8080" \
-    -e "VESPA_QUERY_URL=http://localhost:8080" \
+docker run -d --name marqo --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway \
+    -e MARQO_MAX_CPU_MODEL_MEMORY=1.6 \
+    -e MARQO_ENABLE_BATCH_APIS=true \
+    -e VESPA_CONFIG_URL="host.docker.internal:19071" \
+    -e VESPA_DOCUMENT_URL="host.docker.internal:8080" \
+    -e VESPA_QUERY_URL="host.docker.internal:8080" \
     ${@:+"$@"} "$MARQO_DOCKER_IMAGE" --memory=6g
 set +x
 
