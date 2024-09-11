df8ce9b5cb9ca87ad61a7bd1a26586ddb2967e58
Raynor Chavez <raynorkirksonchavez@gmail.com>
2024-09-11 14:49:12 +0900

    skip languagebind on cuda

--- tests/api_tests/structured_index/test_add_documents.py
+++ tests/api_tests/structured_index/test_add_documents.py
@@ -357,7 +357,7 @@ class TestStructuredAddDocuments(MarqoTestCase):
         assert doc_res['_tensor_facets'][0]["custom_vector_field_1"] == "custom vector text"
         assert doc_res['_tensor_facets'][0]['_embedding'] == [1.0 for _ in range(DEFAULT_DIMENSIONS)]
 
-    #@pytest.mark.skipif(is_cuda_available() is True, reason="GPU test to be investigated")
+    @pytest.mark.skipif(is_cuda_available() is True, reason="GPU test to be investigated")
     def test_add_multimodal_single_documents(self):
         self.removeAllModels()
         documents = [
@@ -392,7 +392,7 @@ class TestStructuredAddDocuments(MarqoTestCase):
             self.assertIn('_embedding', tensor_facets[0])
             self.assertEqual(len(tensor_facets[0]['_embedding']), 768)
 
-    #@pytest.mark.skipif(is_cuda_available() is True, reason="GPU test to be investigated")
+    @pytest.mark.skipif(is_cuda_available() is True, reason="GPU test to be investigated")
     def test_add_documents_with_invalid_media_fields(self):
         self.removeAllModels()
         documents = [
@@ -445,7 +445,7 @@ class TestStructuredAddDocuments(MarqoTestCase):
         with self.assertRaises(MarqoWebError):
             self.client.index(self.structured_languagebind_index_name).get_document(document_id="2")
 
-    #@pytest.mark.skipif(is_cuda_available() is True, reason="GPU test to be investigated")
+    @pytest.mark.skipif(is_cuda_available() is True, reason="GPU test to be investigated")
     def test_add_documents_with_mismatched_media_fields(self):
         self.removeAllModels()
         documents = [
