621ff15ff362925afc59eae2e1b16b8c7db8308c
Raynor Chavez <raynorkirksonchavez@gmail.com>
2024-09-11 16:36:12 +0900

    change to cpu only mark

--- tests/api_tests/structured_index/test_add_documents.py
+++ tests/api_tests/structured_index/test_add_documents.py
@@ -9,12 +9,6 @@ from marqo.errors import MarqoWebError
 
 from tests.marqo_test import MarqoTestCase
 
-def is_cuda_available():
-    try:
-        result = subprocess.run(['nvidia-smi'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return result.returncode == 0
-    except FileNotFoundError:
-        return False
     
 class TestStructuredAddDocuments(MarqoTestCase):
     text_index_name = "add_doc_api_test_structured_index" + str(uuid.uuid4()).replace('-', '')
@@ -357,7 +351,7 @@ class TestStructuredAddDocuments(MarqoTestCase):
         assert doc_res['_tensor_facets'][0]["custom_vector_field_1"] == "custom vector text"
         assert doc_res['_tensor_facets'][0]['_embedding'] == [1.0 for _ in range(DEFAULT_DIMENSIONS)]
 
-    @pytest.mark.skipif(is_cuda_available() is True, reason="GPU test to be investigated")
+    @pytest.mark.cpu_only_test
     def test_add_multimodal_single_documents(self):
         documents = [
             {
@@ -391,7 +385,7 @@ class TestStructuredAddDocuments(MarqoTestCase):
             self.assertIn('_embedding', tensor_facets[0])
             self.assertEqual(len(tensor_facets[0]['_embedding']), 768)
 
-    @pytest.mark.skipif(is_cuda_available() is True, reason="GPU test to be investigated")
+    @pytest.mark.cpu_only_test
     def test_add_documents_with_invalid_media_fields(self):
         documents = [
             {
@@ -443,7 +437,7 @@ class TestStructuredAddDocuments(MarqoTestCase):
         with self.assertRaises(MarqoWebError):
             self.client.index(self.structured_languagebind_index_name).get_document(document_id="2")
 
-    @pytest.mark.skipif(is_cuda_available() is True, reason="GPU test to be investigated")
+    @pytest.mark.cpu_only_test
     def test_add_documents_with_mismatched_media_fields(self):
         documents = [
             {
