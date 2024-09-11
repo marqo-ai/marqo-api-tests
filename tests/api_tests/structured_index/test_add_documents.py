655582ee2ecc974875a2dc0fe3f86b0166a2a156
Li Wan <lwan3@student.unimelb.edu.au>
2024-09-11 20:05:55 +1000

    Remove cpu only

--- tests/api_tests/structured_index/test_add_documents.py
+++ tests/api_tests/structured_index/test_add_documents.py
@@ -351,7 +351,6 @@ class TestStructuredAddDocuments(MarqoTestCase):
         assert doc_res['_tensor_facets'][0]["custom_vector_field_1"] == "custom vector text"
         assert doc_res['_tensor_facets'][0]['_embedding'] == [1.0 for _ in range(DEFAULT_DIMENSIONS)]
 
-    @pytest.mark.cpu_only_test
     def test_add_multimodal_single_documents(self):
         documents = [
             {
@@ -385,7 +384,6 @@ class TestStructuredAddDocuments(MarqoTestCase):
             self.assertIn('_embedding', tensor_facets[0])
             self.assertEqual(len(tensor_facets[0]['_embedding']), 768)
 
-    @pytest.mark.cpu_only_test
     def test_add_documents_with_invalid_media_fields(self):
         documents = [
             {
