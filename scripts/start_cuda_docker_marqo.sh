56a406ea90d2aa124aff38e114f1b2bf680a3763
Yihan Zhao <yihan@marqo.ai>
2024-10-22 15:41:48 +1100

    Fix the error message for concurrent index operation test (#102)
    * Change the error code
    
    * Reduce the max index deployment lock time to 0 so concurrent index operation fail immediately

--- scripts/start_cuda_docker_marqo.sh
+++ scripts/start_cuda_docker_marqo.sh
@@ -15,6 +15,7 @@ docker run -d --name marqo --gpus all --privileged -p 8882:8882 --add-host host.
   -e MARQO_ENABLE_BATCH_APIS=TRUE \
   -e MARQO_MAX_CUDA_MODEL_MEMORY=15 \
   -e MARQO_MAX_CPU_MODEL_MEMORY=15 \
+  -e MARQO_INDEX_DEPLOYMENT_LOCK_TIMEOUT=0 \
     ${@:+"$@"} "$MARQO_DOCKER_IMAGE"
 set +x
 
